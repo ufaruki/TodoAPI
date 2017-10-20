6e5c54786f21404e87a8cf7c044c4939a4161f5a
ufaruki <ufaruki@asicentral.com>
2017-10-20 19:14:03 +0500

    test git remote with vscode

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express'),
   bodyParser = require('body-parser');
 
   mongoose.Promise = global.Promise;
-  mongoose.connect('mongodb://localhost/Tododb', { 'useMongoClient': true });
+  mongoose.connect('mongodb://localhost/Tododb', { 'useMongoClient': true });   
 
  app.use(bodyParser.urlencoded({ extended: true }));
  app.use(bodyParser.json());
